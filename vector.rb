1da77f81a86f2e076ead58f35ccf630b2e35e8a2
e1333 <e1333@PC4-50.cc.matsue-ct.jp>
2017-09-26 11:29:32 +0900

    アトリビュートの追加

--- vector.rb
+++ vector.rb
@@ -1,12 +1,20 @@
 ﻿class Vector
+  attr_accessor :x, :y
+ 
  def initialize(x,y)
   @x=x
   @y=y
  end
+ 
  def to_s
   "(#{@x},#{@y})"
  end
+ 
  def length
  Math.sqrt(@x**2+@y**2)
+ end
+ 
+ def add()
+
  end
 end
